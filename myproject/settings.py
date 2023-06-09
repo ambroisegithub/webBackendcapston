9f37eba29e6953dcebf8dd88fb0bc3128f64cba6
ambroisegithub <muhayimana21@gmail.com>
2023-06-09 17:55:21 +0200

    changes

--- myproject/settings.py
+++ myproject/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'litleLemonApi1',
         'USER':'root',
-        'PASSWORD':'litleLemonApi1234',
+        'PASSWORD':'',
         'HOST':'127.0.0.1',
         'PORT':'3306',
     }
